238005bbb81026d1a03e9fd62ff200a0c12b5ab0
Santiagobv687 <santiagobv687@gmail.com>
2024-10-04 17:25:39 -0500

    importacion

---
+++ bancoUq/src/main/java/co/edu/uniquindio/banco/bancouq/model/CuentaCorriente.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.banco.bancouq.model;
+
+import java.io.Serializable;
+
+public class CuentaCorriente extends Cuenta implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public CuentaCorriente() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ bancoUq/src/main/java/co/edu/uniquindio/banco/bancouq/model/Deposito.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.banco.bancouq.model;
+
+import java.io.Serializable;
+
+public class Deposito extends Transaccion implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public Deposito() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
