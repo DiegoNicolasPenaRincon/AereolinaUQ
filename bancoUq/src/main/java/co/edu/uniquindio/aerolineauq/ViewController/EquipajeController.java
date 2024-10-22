301e5678725f1bbc13adca171b9b40a7e47e4261
Bou <mariac.aranzazur@uqvirtual.edu.co>
2024-10-21 22:26:38 -0500

    Registro de equipaje
    se crea la ventana del registro del equipaje

---
+++ bancoUq/src/main/java/co/edu/uniquindio/aerolineauq/ViewController/EquipajeController.java
@@ -0,0 +1,49 @@
+package co.edu.uniquindio.aerolineauq.ViewController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class EquipajeController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private ComboBox<?> cbTipoEquipaje;
+
+    @FXML
+    private CheckBox ckMascotaNo;
+
+    @FXML
+    private CheckBox ckMascotaSi;
+
+    @FXML
+    private TextField txtNumeroVuelo;
+
+    @FXML
+    private TextField txtPesoMascota;
+
+    @FXML
+    void registrarEquipaje(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
